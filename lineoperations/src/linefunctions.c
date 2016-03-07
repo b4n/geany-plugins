f3354b88c75b6d94d47e3e63a874743863b868b8
Colomban Wendling <ban@herbesfolles.org>
2016-03-07 16:26:19 +0100

    lineoperations: Fix a memory leak found by cppcheck

--- lineoperations/src/linefunctions.c
+++ lineoperations/src/linefunctions.c
@@ -295,8 +295,6 @@ void sortlines(GeanyDocument *doc, gboolean asc) {
 	total_num_chars = sci_get_length(doc->editor->sci);
 	total_num_lines = sci_get_line_count(doc->editor->sci);
 	lines           = g_malloc(sizeof(gchar *) * (total_num_lines+1));
-	new_file        = g_malloc(sizeof(gchar) * (total_num_chars+1));
-	new_file[0]     = '\0';
 
 	/* if file is not empty, ensure that the file ends with newline */
 	if(total_num_lines != 1)
